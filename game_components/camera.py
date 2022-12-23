9c3d96cda8ade104f887fae3e6a459a803972691
rushan <rushan.davlitshin.0809@gmail.com>
2022-12-23 22:06:12 +0500

    Добавление класса камеры

---
+++ game_components/camera.py
@@ -0,0 +1,20 @@
+from game_components import Entity, Wall
+from math_components import Vector
+
+
+class Camera:
+    def __init__(self, window_size: tuple[int, int]):
+        self.dx = self.dy = 0
+        self.width, self.height = window_size
+
+    def update(self, entity: Entity) -> None:
+        self.dx = -(entity.rect.x + entity.rect.w // 2 - self.width // 2)
+        self.dy = -(entity.rect.y + entity.rect.h // 2 - self.height // 2)
+
+    def apply_entity(self, entity: Entity) -> None:
+        x, y = entity.position.x, entity.position.y
+        entity.set_position(x + self.dx, y + self.dy)
+
+    def apply_wall(self, wall: Wall) -> None:
+        wall.rect.x += self.dx
+        wall.rect.y += self.dy
