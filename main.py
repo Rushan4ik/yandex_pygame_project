861df3c165cd9dba52798cd65a2bcd123400a93a
rushan <rushan.davlitshin.0809@gmail.com>
2023-01-05 19:30:47 +0500

    Добавление в main.py возможности, сохранять настройки или не сохранять

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import sys
 import pygame
-
+from json import dump
 from screen_components import SceneManager
 from screen_components.scene_main import MainMenuScene
 from window import Window
+SAVE_SETTINGS = True
 
 
 def start():
+    if not SAVE_SETTINGS:
+        dump({"MAX_VERTICAL_SPEED": 300, "GRAVITY_ACCELERATION": 50, "PLAYER_ANIMATION_SPEED": 0.3,
+              "PLAYER_SPEED": 50, "PLAYER_JUMP_FORCE": 50}, open('configs.json', 'w'))
     pygame.init()
 
 
